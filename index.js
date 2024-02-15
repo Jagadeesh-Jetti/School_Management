5ab57c205cd5d7361b063af536891613c12828bd
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-16 01:15:43 +0530

    feat: added and updated index file

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const initializeDatabase = require("./db");
+const StudentRouter = require("./routers/Student.router");
+const TeacherRouter = require("./routers/Teacher.router");
+
+initializeDatabase();
+
+app.get("/", (req, res) => {
+  res.send("School Management Backend");
+});
+
+app.use("/students", StudentRouter);
+app.use("/teachers", TeacherRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`School Management backend server started on ${PORT} `)
+);
